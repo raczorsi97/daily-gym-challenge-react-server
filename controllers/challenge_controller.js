e72e9c76689e703aafade74de5df040adf166b91
Orsolya.Racz <orsolya.racz@blackline.com>
2019-10-22 16:42:23 +0300

    Some updates

--- controllers/challenge_controller.js
+++ controllers/challenge_controller.js
@@ -6,6 +6,7 @@ const challengeService = require('../services/challenge_service');
 router.get('/all', function (request, response) {
     challengeService.getAllChallenges()
         .then((challenges) => {
+            console.log('Challenges :', challenges);
             response.status(200).json({ 'challenges': challenges });
          }).catch((error) => {
             response.status(500).json({ 'error': error.message });
--- mocks/challenges.js
+++ mocks/challenges.js
@@ -2,16 +2,28 @@ const challenges = [
     {
         'id': 1
         , 'title': 'Stair master challenge'
+        , 'creation_date': '2016.05.12'
         , 'category': 'cardio'
-        , 'dificulty': 'hard' 
-        , 'description': 'Do 60 minutes on stair master...'
+        , 'dificulty': 'hard'
+        , 'short_description': 'If you think you are strong enough for a good cardio session, we are challenging you to do 60 minutes on stair master......'
+        , 'long_description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+        , 'image': 'card_images/stairmaster.png'
     }
     , {
         'id': 2
         , 'title': 'Jumping jack challenge'
         , 'category': 'cardio'
         , 'dificulty': 'hard' 
-        , 'description': 'Do 100 of jumping jacks...'
+        , 'short_description': 'A jumping jack is a physical jumping exercise performed by jumping to a position with the legs spread wide and the hands touching overhead. Let`s do it for 10 minutes!'
+        , 'image': 'card_images/jumpingjacks.png'
+    }
+    , {
+        'id': 3
+        , 'title': 'Burpee challenge'
+        , 'category': 'cardio'
+        , 'dificulty': 'hard' 
+        , 'short_description': 'The burpee, or squat thrust[citation needed], is a full body exercise used in strength training and as an aerobic exercise. I bet you can`t do 50 by one rep.'
+        , 'image': 'card_images/burpees.png'
     }
 ];
 module.exports = challenges;
